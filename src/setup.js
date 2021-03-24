9649a3bb8b167c0556de235033fd748108a0866a
didier <didierlennon@gmail.com>
2021-03-24 22:20:34 +0100

    Update example to not overwrite the headers object

--- src/setup.js
+++ src/setup.js
@@ -2,8 +2,24 @@
  * The error seems to occur when returning _something_, in this case `headers`, but it could happen with `context` as well.
  */
 export async function prepare({ headers }) {
+  // Destructure every part of the headers
+  const { host, cookie, accept } = headers;
+  const userAgent = headers['user-agent'];
+  const contentType = headers['content-type'];
+  const contentLength = headers['content-length'];
+
   return {
-    // Try commenting out the next line. The endpoint should work again.
-    headers,
+    // Returning headers direcly causes an error, presumably because of the `content-length` header.
+    // headers,
+
+    // However, when returning the headers individually (re: not overwriting) everything works fine.
+    headers: {
+      host,
+      cookie,
+      userAgent,
+      contentType,
+      contentLength,
+      accept,
+    },
   };
 }
